07b23756389738b9381e3b3597829e7a680257e7
tulliogomes <tulliogomes@hotmail.com>
2018-06-28 14:22:58 -0300

    Fixing Approve and Remove Comment

--- blog/views.py
+++ blog/views.py
@@ -76,12 +76,12 @@ def post_remove(request, pk):
 
 @login_required
 def comment_approve(request, pk):
-    comment = get_object_or_404(Post, pk=pk)
+    comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
-    comment = get_object_or_404(Post, pk=pk)
+    comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
